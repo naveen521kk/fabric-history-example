fcd7fdee7463f9222483a44759e39c36fa2716af
Naveen M K <naveen521kk@gmail.com>
2024-11-29 23:25:20 +0530

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,146 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React from "react";
+
+import * as fabric from "fabric";
+import { FabricCanvasHistory } from "@naveen521kk/fabric-history";
+
+import { Button } from "@/components/ui/button";
+import {
+  Undo2Icon,
+  Redo2Icon,
+  DeleteIcon,
+  PlusCircle,
+  RectangleHorizontal,
+} from "lucide-react";
+
+export default function HomePage() {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasContainerRef = React.useRef<HTMLDivElement>(null);
+
+  const [canvas, setCanvas] = React.useState<fabric.Canvas | null>(null);
+  const [history, setHistory] = React.useState<FabricCanvasHistory | null>(
+    null
+  );
+
+  const [reRenderCount, setReRenderCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!canvasRef.current) return;
+    if (!canvasContainerRef.current) return;
+
+    const container = canvasContainerRef.current;
+
+    if (!container) return;
+
+    const { width, height } = container.getBoundingClientRect();
+
+    const canvas = new fabric.Canvas(canvasRef.current);
+
+    const history = new FabricCanvasHistory(canvas, () => {
+      setReRenderCount((count) => count + 1);
+    });
+
+    history.init();
+
+    canvas.setDimensions({
+      width,
+      height,
+    });
+
+    setCanvas(canvas);
+    setHistory(history);
+
+    return () => {
+      canvas.dispose();
+    };
+  }, [canvasRef]);
+
+  console.log(
+    reRenderCount,
+    history?.canPerformUndo(),
+    history?.canPerformRedo()
+  );
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      className="font-[family-name:var(--font-geist-sans)] flex flex-col justify-center items-center w-screen h-screen"
+      ref={canvasContainerRef}
+    >
+      <div className="absolute top-4 left-4 z-50 flex gap-4 flex-row">
+        <div key={reRenderCount} className="flex gap-4">
+          <Button
+            disabled={!history?.canPerformUndo()}
+            onClick={() => {
+              history?.undo();
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <Undo2Icon /> Undo
+          </Button>
+          <Button
+            disabled={!history?.canPerformRedo()}
+            onClick={() => {
+              history?.redo();
+            }}
           >
-            Read our docs
-          </a>
+            <Redo2Icon /> Redo
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          onClick={() => {
+            history?.clear();
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <DeleteIcon /> Clear history
+        </Button>
+        <Button
+          onClick={() => {
+            canvas?.clear();
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <DeleteIcon /> Clear canvas
+        </Button>
+        <Button
+          onClick={() => {
+            const rect = new fabric.Rect({
+              left: 100,
+              top: 100,
+              fill: "red",
+              width: 100,
+              height: 100,
+            });
+            canvas?.add(rect);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <RectangleHorizontal /> Rectangle
+        </Button>
+        <Button
+          onClick={() => {
+            const circle = new fabric.Circle({
+              left: 100,
+              top: 100,
+              fill: "blue",
+              radius: 100,
+            });
+            canvas?.add(circle);
+          }}
+        >
+          <PlusCircle /> Circle
+        </Button>
+        <Button
+          onClick={() => {
+            const activeObjects = canvas?.getActiveObjects();
+            if (activeObjects) {
+              activeObjects.forEach((object) => {
+                canvas?.remove(object);
+              });
+            }
+          }}
+        >
+          Delete selected
+        </Button>
+      </div>
+      <canvas ref={canvasRef} />
     </div>
   );
 }
